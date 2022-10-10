a07ee5721cb03fe98b38ca8403d6e08f34278010
Modenero Support <support@modenero.com>
2022-10-10 04:04:20 -0400

    Initialize development portal.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Modenero Development
+
+### https://modenero.dev
+
+![Modenero logo](https://github.com/modenero/assets/blob/master/logo.png)
---
+++ web/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* Import tailwind. */
+import 'tailwindcss/tailwind.css'
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
